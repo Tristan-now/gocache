860f6b63b236486dd7634e605bffa1f4417a2925
arco_notebook <3481554467@qq.com>
2024-02-24 11:40:21 +0800

    day6_singleflight_1

--- geecache/singleflight/singleflight_test.go
+++ geecache/singleflight/singleflight_test.go
@@ -1,22 +1,37 @@
 package singleflight
 
 import (
+	"fmt"
 	"testing"
 )
 
-var testCast = map[int]int{
-	1: 1,
-	2: 2,
-	3: 3,
-}
+var s = "init"
+var x = 1
 
 func TestDo(t *testing.T) {
 	var g Group
-	v, err := g.Do("key", func() (interface{}, error) {
-		return "bar", nil
-	})
+	//v, err := g.Do("key", func() (interface{}, error) {
+	//	return "bar", nil
+	//})
+	//
+	//if v != "bar" || err != nil {
+	//	t.Errorf("Do v = %v, error = %v", v, err)
+	//}
 
-	if v != "bar" || err != nil {
-		t.Errorf("Do v = %v, error = %v", v, err)
+	for i := 0; i < 100; i++ {
+		go g.Do(s, func() (interface{}, error) {
+			x++
+			fmt.Println(x)
+			return nil, nil
+		})
 	}
+
+	//for i := 0; i < 100; i++ {
+	//	g.Do(s, func() (interface{}, error) {
+	//		x++
+	//		println(x)
+	//		return nil, nil
+	//	})
+	//}
+
 }
